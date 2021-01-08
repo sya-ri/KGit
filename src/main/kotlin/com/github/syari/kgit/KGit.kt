233f90c0d1375cf80d03f104a0dbdf4356023688
sya-ri <sya79lua@gmail.com>
2021-01-08 18:51:43 +0900

    feat: Add KGit::open,shutdown,wrap

--- src/main/kotlin/com/github/syari/kgit/KGit.kt
+++ src/main/kotlin/com/github/syari/kgit/KGit.kt
@@ -4,12 +4,25 @@ import com.github.syari.kgit.api.KCloneCommand
 import com.github.syari.kgit.api.KInitCommand
 import com.github.syari.kgit.api.KLsRemoteCommand
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.util.FS
+import java.io.File
 
 /**
  * @see Git
  */
 class KGit(private val asJ: Git) {
     companion object {
+        /**
+         * @see Git.open
+         */
+        fun open(dir: File, fs: FS = FS.DETECTED) = KGit(Git.open(dir, fs))
+
+        /**
+         * @see Git.open
+         */
+        fun wrap(repo: Repository) = KGit(repo)
+
         /**
          * @see Git.cloneRepository
          */
@@ -24,5 +37,12 @@ class KGit(private val asJ: Git) {
          * @see Git.init
          */
         inline fun init(action: KInitCommand.() -> Unit = {}) = KInitCommand().apply(action).call()
+
+        /**
+         * @see Git.shutdown
+         */
+        fun shutdown() = Git.shutdown()
     }
+
+    constructor(repo: Repository): this(Git(repo))
 }
